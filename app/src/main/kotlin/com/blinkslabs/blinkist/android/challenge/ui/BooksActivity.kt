f0bf9f22b6f68ed7e9fa39e187dcd9169fdcd491
Jonathan Aranguri <arangurim@gmail.com>
2023-05-29 23:30:05 -0300

    feat: Adding collapsing toolbar with Blinkist design

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/ui/BooksActivity.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/ui/BooksActivity.kt
@@ -9,31 +9,35 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.blinkslabs.blinkist.android.challenge.BlinkistChallengeApplication
 import com.blinkslabs.blinkist.android.challenge.R
 import com.blinkslabs.blinkist.android.challenge.data.model.BookSection
+import com.google.android.material.appbar.AppBarLayout
 import javax.inject.Inject
 
-class BooksActivity : AppCompatActivity() {
+class BooksActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
 
-    @Inject lateinit var booksViewModelFactory: BooksViewModelFactory
+    private lateinit var appBarLayout: AppBarLayout
+
+    @Inject
+    lateinit var booksViewModelFactory: BooksViewModelFactory
 
     private val viewModel by viewModels<BooksViewModel> { booksViewModelFactory }
 
     private lateinit var recyclerAdapter: BookSectionsRecyclerAdapter
 
-    private lateinit var recyclerView : RecyclerView
-    private lateinit var swipeRefreshLayout : SwipeRefreshLayout
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
         recyclerView = findViewById(R.id.bookSectionsRecyclerView)
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+        appBarLayout = findViewById(R.id.appBarLayout)
 
         (application as BlinkistChallengeApplication).component.inject(this)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerAdapter = BookSectionsRecyclerAdapter()
         recyclerView.adapter = recyclerAdapter
-
         swipeRefreshLayout.setOnRefreshListener { viewModel.refreshBooks() }
 
         viewModel.books().observe(this) { books ->
@@ -56,5 +60,19 @@ class BooksActivity : AppCompatActivity() {
     private fun showBooks(books: List<BookSection>) {
         recyclerAdapter.setItems(books)
         hideLoading()
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, i: Int) {
+        swipeRefreshLayout.isEnabled = i == 0
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appBarLayout.addOnOffsetChangedListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        appBarLayout.removeOnOffsetChangedListener(this)
     }
 }
